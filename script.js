638e70a29bd99d9ef21fa3c439e5ad68c644ec36
MansiAy <135643246+MansiAy@users.noreply.github.com>
2023-07-10 14:03:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var tl=gsap.timeline(
+//     {
+//         scrollTrigger:{
+//             trigger:"#first",
+//             start:"100% 99%",
+//             pin:true,
+//             end:"20% 40%",
+//             markers:true,
+//             scrub:2
+//             }
+//     }); 
+//     tl
+// .to("#center-img img",{
+//     top:"-30%",
+//     stagger:.3,
+//     duration:10
+// })
+    // .to("#pic1",{
+    //     top:"-100%",
+    //     ease:Power0,
+    //     delay:2,
+    //     duration:9
+        
+    // },"a")
+    // .to("#pic2",{
+    //     top:"-80",
+    //     delay:2,
+    //     duration:9,
+    //     ease:Power0
+    // },"a")
+    // .to("#pic3",{
+    //     top:"10%",
+    //     delay:5,
+    //     duration:6,
+    //     ease:Power0
+    // },"a")
+
+
